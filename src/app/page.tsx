9dc2e10f424f0a2acdec4a932120e87c09b05279
Paylak Navasardyan <paylak2002n@gmail.com>
2025-03-04 13:34:13 +0400

    Remove 'use client' directive and refactor for server-side rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,36 @@
-"use client";
-
-import axios from "axios";
 import classes from "./page.module.css";
-import React, { useEffect, useState, useCallback, FC } from "react";
-import { IUser, HomeProps } from "./types/types";
+import { IUser } from "./types/types";
 
-export default function Home<T>(props: HomeProps<T>) {
-  const [users, setUsers] = useState<IUser[]>([
-    { id: 11, name: "Jonathan Jonson", email: "JJ-blakc@gmail.com" },
-    { id: 12, name: "Ben Potter", email: "Potter_Ben@gmail.com" },
-    { id: 13, name: "Andrew Scott Jr.", email: "ScottJr@gmail.com" }
-  ]); 
-  
-  const [loading, setLoading] = useState<boolean>(true);
+async function fetchUsers(): Promise<IUser[] | undefined> {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const apiUsers: IUser[] = await res.json();
 
-  const fetchUser = useCallback(async () => {
-    try {
-      const res = await axios.get<IUser[]>("https://jsonplaceholder.typicode.com/users");
-      console.log("Fetched users:", res.data);
-      
-      setUsers((prevUsers) => [...res.data, ...prevUsers]);
-    } catch (error) {
-      console.error("Error fetching users:", error);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+    const predefinedUsers: IUser[] = [
+      { id: 11, name: "Jonathan Jonson", email: "JJ-blakc@gmail.com" },
+      { id: 12, name: "Ben Potter", email: "Potter_Ben@gmail.com" },
+      { id: 13, name: "Andrew Scott Jr", email: "ScottJr@gmail.com" }
+    ];
 
-  useEffect(() => {
-    fetchUser();
-  }, [fetchUser]);
+    return [...apiUsers, ...predefinedUsers];
+  } catch(error) {
+    console.error(console.error("Error fetching users:", error)); 
+  }
+}
+
+export default async function Home() {
+  const users = await fetchUsers(); 
 
   return (
     <div className={classes.page}>
-      {loading ? (
-        <p>Loading users...</p>
-      ) : (
-        users.map((user) => (
-          <div key={user.id} className={classes.pageFields}>
-            <h2>{user.name}</h2>
-          </div>
-        ))
-      )}
+      {users ?
+      users.map((user) => (
+        <div key={user.id} className={classes.pageFields}>
+          <h2>{user.name}</h2>
+        </div>
+      )) : 
+      <p>Loading ...</p>
+      }
     </div>
   );
 }
