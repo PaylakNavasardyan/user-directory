d528c3692e51df1e374cc474613cf4f1098d3f75
Paylak Navasardyan <paylak2002n@gmail.com>
2025-03-06 21:46:01 +0400

    Redirect to a specific user page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import  Link  from "next/link";
 import classes from "./page.module.css";
 import { IUser } from "./types/types";
 
-async function fetchUsers(): Promise<IUser[] | undefined> {
+async function fetchUsers(): Promise<IUser[] | null> {
   try {
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const apiUsers: IUser[] = await res.json();
@@ -15,6 +16,7 @@ async function fetchUsers(): Promise<IUser[] | undefined> {
     return [...apiUsers, ...predefinedUsers];
   } catch(error) {
     console.error(console.error("Error fetching users:", error)); 
+    return null;
   }
 }
 
@@ -25,9 +27,11 @@ export default async function Home() {
     <div className={classes.page}>
       {users ?
       users.map((user) => (
-        <div key={user.id} className={classes.pageFields}>
-          <h2>{user.name}</h2>
-        </div>
+        <Link key={user.id} href={`/user/${user.id}`} className={classes.pageFields}> 
+          <div>
+            <h2>{user.name}</h2>
+          </div>
+        </Link>
       )) : 
       <p>Loading ...</p>
       }
