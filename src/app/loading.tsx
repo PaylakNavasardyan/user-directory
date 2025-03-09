ed11854cab82ccb5ce18feb5e5f4a1cc9577cb2e
Paylak Navasardyan <paylak2002n@gmail.com>
2025-03-09 21:02:31 +0400

    New page for loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+    return <p style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        fontSize: '40px',
+        color: '#004242FF'
+    }}><strong>Loading...</strong> </p>
+}
